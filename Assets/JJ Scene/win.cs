5cf1963bd4ca397bf371f325df40881c091df8cd
MUKILTEO\1713529 <1713529@mukilteo.wednet.edu>
2024-03-22 10:16:00 -0700

    mad first puzzle

---
+++ Assets/JJ Scene/win.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class win : MonoBehaviour
+{
+    public GameObject PipesHolder;
+    public GameObject[] Pipes;
+
+    public int totalPipes = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalPipes = PipesHolder.transform.childCount;
+
+        Pipes = new GameObject[totalPipes];
+
+        for (int i = 0; i < Pipes.Length; i++) 
+        {
+            Pipes[i] = PipesHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
