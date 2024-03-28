80480616127ac1168667035920c7ac74e65cb9bb
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-03-28 10:11:50 -0700

    Lazer beams

--- Assets/Scenes/Andrew/FlickerLight.cs
+++ Assets/Scenes/Andrew/FlickerLight.cs
@@ -6,11 +6,13 @@ using UnityEngine.Rendering.Universal;
 
 public class FlickerLight : MonoBehaviour
 {
-    public Light2D _light;
-
+    private GameObject[] _light;
     public float MinTime;
     public float MaxTime;
     public float Timer;
+
+    [SerializeField, HideInInspector]
+    private ErrorsForRooms errors;
 
     //public AudioSource AudioSource; //IDK yet...nvm, figured it out. It is the source that will play the noise. DUH!
     //public AudioClip lightAudio; //The beeping sound for the error occuring
@@ -24,6 +26,15 @@ public class FlickerLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(errors.RoomNumber > 0)
+        {
+            //_light = errors._mapLights[errors.RoomNumber];ss
+        }
+        else
+        {
+            _light = GameObject.FindGameObjectsWithTag("ErrorLight");
+        }
+
         LightFlicker();
     }
 
@@ -34,7 +45,7 @@ public class FlickerLight : MonoBehaviour
 
         if(Timer<= 0)
         {
-            _light.enabled = !_light.enabled;
+            //_light.enabled = !_light.enabled;
             Timer = Random.Range(MinTime, MaxTime);
             //AudioSource.PlayOneShot(lightAudio);
         }
