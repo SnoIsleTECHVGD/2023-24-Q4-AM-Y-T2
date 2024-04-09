c26d6acae141403fa0a61f390aed4c08aa563f4d
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-04-09 08:26:35 -0700

    More stuff

--- Assets/Scenes/Andrew/LampFlicker.cs
+++ Assets/Scenes/Andrew/LampFlicker.cs
@@ -7,10 +7,20 @@ using UnityEngine.Rendering.Universal;
 public class LampFlicker : MonoBehaviour
 {
     public Light2D Lamp;
+    private float random;
+
+    private void Start()
+    {
+        random = Random.Range(0.0f, 20f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Lamp.intensity = Mathf.PingPong(Time.time, 6f);
+        float maxInt = 8.2f;
+        float minInt = 1.0f;
+        float noise = Mathf.PerlinNoise(random, Time.time);
+
+        Lamp.intensity = Mathf.Lerp(minInt,maxInt, noise);
     }
 }
