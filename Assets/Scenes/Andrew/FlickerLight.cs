90f1e60a15ff42c577e992ad484361dd69d95152
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-04-10 09:46:52 -0700

    Potateos

--- Assets/Scenes/Andrew/FlickerLight.cs
+++ Assets/Scenes/Andrew/FlickerLight.cs
@@ -1,44 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class FlickerLight : MonoBehaviour
 {
-    public float MinTime;
-    public float MaxTime;
-    public float Timer;
-
+    private Light2D Light;
     private ErrorsForRooms errors;
-
-    //public AudioSource AudioSource; //IDK yet...nvm, figured it out. It is the source that will play the noise. DUH!
-    //public AudioClip lightAudio; //The beeping sound for the error occuring
+    private float random;
 
     // Start is called before the first frame update
     void Start()
     {
-        Timer = Random.Range(MinTime, MaxTime);
+        random = Random.Range(0.0f, 20f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(errors.IsThereError)
-        {
-            if(errors.isMontiorOn)
-                LightFlicker(errors._mapLights[errors.RoomNumber]);
-            if(!errors.isMontiorOn)
-                LightFlicker(errors._mapLights[7]);
-        }
-    }
+        if(errors.isMontiorOn)
+            Light = errors._mapLights[errors.RoomNumber];
+        if (!errors.isMontiorOn)
+            Light = errors._mapLights[7];
 
-    void LightFlicker(Light2D _light)
-    {
-        if(Timer > 0)
-            Timer -= Time.deltaTime;
-        else
-        {
-            _light.enabled = !_light.enabled;
-            Timer = Random.Range(MinTime, MaxTime);
-            //AudioSource.PlayOneShot(lightAudio);
-        }
+        float maxInt = 15.2f;
+        float minInt = 2.0f;
+        float noise = Mathf.PerlinNoise(random, Time.time * 20);
+
+        Light.intensity = Mathf.Lerp(minInt, maxInt, noise);
     }
 }
