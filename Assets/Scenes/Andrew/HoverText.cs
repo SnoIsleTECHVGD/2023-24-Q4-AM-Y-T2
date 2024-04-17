161e0806489c257b88dee76a0cfe751104c8953a
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-04-17 10:12:46 -0700

    Banana

---
+++ Assets/Scenes/Andrew/HoverText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HoverText : MonoBehaviour
+{
+    public string hoverText;
+    public bool isHovering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var touchPos = new Vector2(wp.x, wp.y);
+
+        Collider2D hit = Physics2D.OverlapPoint(touchPos);
+
+        hoverText = hit.transform.name;
+    }
+}
