209b4e709c6a4969beeb2b4c3dae731fdcc1bb88
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-05-06 09:22:59 -0700

    Finished the map, and started the polish of hover

--- Assets/Scenes/Andrew/HoverText.cs
+++ Assets/Scenes/Andrew/HoverText.cs
@@ -22,37 +22,40 @@ public class HoverText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var touchPos = new Vector2(wp.x, wp.y);
-
-        Collider2D hit = Physics2D.OverlapPoint(touchPos);
-
-        if (hit && hit.transform.gameObject.tag == "Doorways")
-        {
-            if (hit.transform.position.x > 0) //Left side door
-            {
-                textBox = LeftextBox;
-                text = textBox.GetComponentInChildren<TMP_Text>();
-            }
-            if (hit.transform.position.x < 0) //Right side door
-            {
-                textBox = RightextBox;
-                text = textBox.GetComponentInChildren<TMP_Text>();
-            }
-
-            textBox.gameObject.SetActive(true);
-            Debug.LogWarning("WE DID SOMETHING CORRECT.");
-            hoverText = hit.transform.gameObject.name;
-            Debug.Log(hoverText);
-        }
-        if(!hit)
-            textBox.gameObject.SetActive(false);
-
-        text.text = hoverText;
-
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Debug.Break();
         }
+
+        Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var touchPos = new Vector2(wp.x, wp.y);
+
+        Collider2D hit = Physics2D.OverlapBox(Input.mousePosition, new Vector2(5,5), 0);
+
+        if(hit)
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        //if (hit && hit.transform.gameObject.tag == "Doorways")
+        //{
+        //    if (hit.transform.position.x > 0) //Left side door
+        //    {
+        //        textBox = LeftextBox;
+        //        text = textBox.GetComponentInChildren<TMP_Text>();
+        //    }
+        //    if (hit.transform.position.x < 0) //Right side door
+        //    {
+        //        textBox = RightextBox;
+        //        text = textBox.GetComponentInChildren<TMP_Text>();
+        //    }
+
+        //    textBox.gameObject.SetActive(true);
+        //    Debug.LogWarning("WE DID SOMETHING CORRECT.");
+        //    hoverText = hit.transform.gameObject.name;
+        //    Debug.Log(hoverText);
+        //}
+        //if(!hit)
+        //    textBox.gameObject.SetActive(false);
     }
 }
