9834668df805f2b88d7b2817d77c5aa28f202e20
MUKILTEO\1713915 <1713915@mukilteo.wednet.edu>
2024-05-06 10:13:08 -0700

    Still hovering text

--- Assets/Scenes/Andrew/HoverText.cs
+++ Assets/Scenes/Andrew/HoverText.cs
@@ -1,4 +1,6 @@
+using System.Linq.Expressions;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,56 +8,32 @@ using UnityEngine.UI;
 public class HoverText : MonoBehaviour
 {
     public string hoverText;
-    private TMP_Text text;
-    private Image textBox;
-    public Image LeftextBox;
-    public Image RightextBox;
-
-    private Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Cursor.visible = false;
-    }
+    public TMP_Text text;
+    public Image textBox;
+    public Canvas potatoCanvas;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        ////var screenPoint = Vector3(Input.mousePosition);
+        //screenPoint.z = 10.0f;
+        //textBox.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+
+
+        if (hit.collider == null)
         {
-            Debug.Break();
+            textBox.gameObject.SetActive(false);
+        }
+        if(hit.collider != null)
+        {
+            textBox.gameObject.SetActive(true);
+            hoverText = hit.transform.gameObject.name;
+            Debug.Log(hit.transform.gameObject.name);
+
+            text.SetText(hoverText);
         }
 
-        Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var touchPos = new Vector2(wp.x, wp.y);
-
-        Collider2D hit = Physics2D.OverlapBox(Input.mousePosition, new Vector2(5,5), 0);
-
-        if(hit)
-        {
-            Debug.Log(hit.transform.name);
-        }
-
-        //if (hit && hit.transform.gameObject.tag == "Doorways")
-        //{
-        //    if (hit.transform.position.x > 0) //Left side door
-        //    {
-        //        textBox = LeftextBox;
-        //        text = textBox.GetComponentInChildren<TMP_Text>();
-        //    }
-        //    if (hit.transform.position.x < 0) //Right side door
-        //    {
-        //        textBox = RightextBox;
-        //        text = textBox.GetComponentInChildren<TMP_Text>();
-        //    }
-
-        //    textBox.gameObject.SetActive(true);
-        //    Debug.LogWarning("WE DID SOMETHING CORRECT.");
-        //    hoverText = hit.transform.gameObject.name;
-        //    Debug.Log(hoverText);
-        //}
-        //if(!hit)
-        //    textBox.gameObject.SetActive(false);
     }
 }
