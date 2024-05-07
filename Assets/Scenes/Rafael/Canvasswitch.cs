d83eca3aaeb09a8d21f59a6daf9853c59f7ca0c4
MUKILTEO\1713331 <RGJackdawnington@gmail.com>
2024-05-07 08:57:44 -0700

    Making sure of something

---
+++ Assets/Scenes/Rafael/Canvasswitch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvasswitch : MonoBehaviour
+{
+    public Canvas menucanvas;
+    public Canvas tutorialmenu;
+    
+  void GoTutorial()
+    {
+
+
+
+    }
+
+    void GoMenu()
+    {
+
+
+
+    }
+
+
+
+}
